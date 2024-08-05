c6559a51105b3ba41bd72b38b005d488bfe90b9e
sai-sasank09 <95159792+sai-sasank09@users.noreply.github.com>
2024-08-06 00:14:52 +0530

    Add files via upload

---
+++ Week 1/Data Structures/TaskManagementSystem/TaskNode.java
@@ -0,0 +1,10 @@
+package TaskManagementSystem;
+public class TaskNode {
+    Task task;
+    TaskNode next;
+
+    public TaskNode(Task task) {
+        this.task = task;
+        this.next = null;
+    }
+}
